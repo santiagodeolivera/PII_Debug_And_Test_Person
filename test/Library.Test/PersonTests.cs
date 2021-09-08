b6e8b198be03bc7036e9e615ee1fca1bda509ee7
Santiago De Olivera <santiagodeolivera@gmail.com>
2021-09-08 13:18:12 -0300

    Exercise resolution

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -6,16 +6,76 @@ namespace Tests
 {
     public class Tests
     {
+        private string initialName;
+        private string initialId;
+        private Person person;
+
         [SetUp]
         public void Setup()
         {
-            // Insertá tu código de inicialización aquí
+            this.initialName = "Santiago De Olivera";
+            this.initialId = "52672887";
+            this.person = new Person(initialName, initialId);
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void TestNameAssignment()
         {
-            // Insertá tu código  de pruebaaquí
+            string newName = "Roberto Sánchez";
+
+            this.person.Name = newName;
+
+            Assert.AreEqual(this.person.Name, newName);
+        }
+
+        [Test]
+        public void TestNameValidationNull()
+        {
+            string newName = null;
+
+            this.person.Name = newName;
+
+            Assert.AreEqual(this.person.Name, initialName);
+        }
+
+        [Test]
+        public void TestNameValidationEmpty()
+        {
+            string newName = "";
+
+            this.person.Name = newName;
+
+            Assert.AreEqual(this.person.Name, initialName);
+        }
+
+        [Test]
+        public void TestIdAssignment()
+        {
+            string newId = "00000000";
+
+            this.person.ID = newId;
+
+            Assert.AreEqual(this.person.ID, newId);
+        }
+
+        [Test]
+        public void TestIdValidationLength()
+        {
+            string newId = "0000001";
+
+            this.person.ID = newId;
+
+            Assert.AreEqual(this.person.ID, initialId);
+        }
+
+        [Test]
+        public void TestIdValidationCheck()
+        {
+            string newId = "00000001";
+
+            this.person.ID = newId;
+
+            Assert.AreEqual(this.person.ID, initialId);
         }
     }
 }
